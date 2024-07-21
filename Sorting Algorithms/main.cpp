15abb216b9072cd92860b45bb3fdcac0231df6ab
kapilamayank <kapilamayank25@gmail.com>
2024-07-22 00:38:01 +0530

    added user interface

--- Sorting Algorithms/main.cpp
+++ Sorting Algorithms/main.cpp
@@ -5,9 +5,10 @@
 
 
 main() {
+	std::cout << "Welcome to Sorting Algorithm Visualizer!" << std::endl << std::endl;
 	
 	int n; int *inputArray;
-	std::cout << "Enter the box size (<=12): ";
+	std::cout << "Enter the array size (<=12): ";
 	std::cin >> n;
 	
 	if (n > 12) {
@@ -21,12 +22,35 @@ main() {
 		std::cin >> inputArray[i];
 	}
 	
+	std::cout << std::endl;
+	std::cout << "Choose Algorithm: " << std::endl;
+	std::cout << "\t" << "[1] Bubble Sort" << std::endl;
+	std::cout << "\t" << "[2] Insertion Sort" << std::endl;
+	std::cout << "\t" << "[3] Selection Sort" << std::endl;
+	std::cout << "Enter Code: ";
+	int code;
+	std::cin >> code;
+	
 	initwindow(1200, 1000);
 	
 	
 	std::vector<ArrayBox *> arr = drawArray(inputArray, n);
 	delay(500);
-	bubbleSort(arr);
+	
+	switch(code) {
+		case 1: 
+			bubbleSort(arr);
+			break;
+		case 2:
+			insertionSort(arr);
+			break;
+		case 3:
+			selectionSort(arr);
+			break;
+		default:
+			std::cout << "Wrong code !" << std::endl;
+			exit(1);
+	}
 	
 	getch();
 }
